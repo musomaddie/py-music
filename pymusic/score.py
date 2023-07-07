cedbb45aa07589e29fbe4e1f08e588913ba3b2e2
Madeleine Wagner <musomaddy@gmail.com>
2023-07-07 16:03:13 +1000

    Using lxml and beginning to build up the score object.

--- pymusic/score.py
+++ pymusic/score.py
@@ -1,6 +1,7 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
-from bs4 import BeautifulSoup
+import lxml
+from lxml import etree
 
 
 @dataclass
@@ -10,16 +11,41 @@ class Score:
     """
     title: str
 
+
+@dataclass
+class ScoreBuilder:
+    _og_xml: lxml.etree._ElementTree
+    _title: str = ""
+    _additional_info: dict[str, str] = field(default_factory=dict)
+
+    def _find_title(self):
+        """ Finds and adds the title from the xml."""
+        root = self._og_xml.getroot()
+        # find work
+        for parent_element in root.iter("work"):
+            for child_element in root.iter("work-title"):
+                self._title = child_element.text
+
+    def _process_children(self):
+        root = self._og_xml.getroot()
+        for child in root:
+            print(child)
+
     @staticmethod
     def create_from_musicxml_file(filename):
-        with open(filename) as f:
-            soup = BeautifulSoup(f, "xml")
-        score_title = soup.work.find("work-title")
-
-        print(score_title.name)
-        print(score_title.attrs)
-        print(score_title.children)
+        builder = ScoreBuilder(etree.parse(filename))
+        builder._find_title()
+        print(builder)
+        builder._process_children()
+        # root = etree.parse(filename).getroot()
+        #
+        # for child in root:
+        #     print(child.tag)
+        #     print(type(child))
+        # print(score_title.name)
+        # print(score_title.attrs)
+        # print(score_title.children)
 
 
 if __name__ == '__main__':
-    Score.create_from_musicxml_file("../tests/realexamples/lavender haze.musicxml")
+    ScoreBuilder.create_from_musicxml_file("../tests/realexamples/lavender haze.musicxml")
