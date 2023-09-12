547328232934d746f59111a8a1073a54425bce49
Maddie Wagner <musomaddy@gmail.com>
2023-09-12 13:54:54 +1000

    Created a list of all possible modes (scales).

--- tests/pymusic/key/test_created_mode.py
+++ tests/pymusic/key/test_created_mode.py
@@ -1,16 +1,3 @@
-# class TestPartBuilder:
-#     def test_part_id(self):
-#         builder = PartBuilder.create_from_part_list_xml(PART_ELEMENT)
-#         assert builder.part_id == "P1"
-#
-#     def test_part_name(self):
-#         builder = PartBuilder.create_from_part_list_xml(PART_ELEMENT)
-#         assert builder._part_name == PART_NAME
-#
-#     def test_part_abbr(self):
-#         builder = PartBuilder.create_from_part_list_xml(PART_ELEMENT)
-#         assert builder._part_abbr == PART_ABBR
-#
 import pytest
 
 from pymusic.key import MAJOR, MINOR, HARMONIC_MINOR, IONIAN, DORIAN, LYDIAN, PHRYGIAN, MIXOLYDIAN, AEOLIAN, LOCRIAN
