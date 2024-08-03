9d060e04bd838dfcdf48dfae971ce15b40d29ad3
Madeleine Wagner <musomaddy@gmail.com>
2024-08-03 11:48:23 +1000

    Added note name enum file back

---
+++ pymusic/pitch/notes/note_name.py
@@ -0,0 +1,20 @@
+""" An enum for the note names, which also allows us to look up and return some other stuff about it. """
+
+from enum import Enum
+
+
+class NoteName(Enum):
+    """	
+    The names of each note in an octave as defined by `ASPN`_.	
+    Each octave starts at C, so the value assigned by the enum reflects this. Internal	
+    implementation of the `step data type`_ defined in Music XML.	
+    .. _`ASPN`: https://viva.pressbooks.pub/openmusictheory/chapter/aspn/	
+    .. _`step data type`: https://www.w3.org/2021/06/musicxml40/musicxml-reference/data-types/step/	
+    """
+    C = 1
+    D = 2
+    E = 3
+    F = 4
+    G = 5
+    A = 6
+    B = 7
