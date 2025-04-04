f804aeeeaf7bfa698d87f6d5ac18b2e934cd3224
Madeleine Wagner <musomaddy@gmail.com>
2025-04-05 08:16:32 +1100

    Added a "create_xml_from_file" method to test util.

--- tests/__init__.py
+++ tests/__init__.py
@@ -7,3 +7,7 @@ from lxml import etree
 def create_xml(content: str) -> etree.Element:
     """ Given a string representing the xml, returns it as an etree element."""
     return etree.parse(BytesIO(content.encode())).getroot()
+
+
+def create_xml_from_file(filename: str) -> etree.Element:
+    return etree.parse(filename).getroot()
--- tests/pymusic/note/test_grace_note.py
+++ tests/pymusic/note/test_grace_note.py
@@ -1,9 +1,9 @@
 """ Tests for the grace note xml item. """
 
 import pytest
-from lxml import etree
 
 from pymusic.note.grace_note import GraceNote
+from tests import create_xml_from_file
 
 
 @pytest.mark.parametrize(
@@ -16,5 +16,6 @@ from pymusic.note.grace_note import GraceNote
     ]
 )
 def test_single_grace_note(slash, slur, expected_glance):
-    music_xml_fn = f"tests/resources/grace_notes/single_notes/slash_{slash}_slur_{slur}.musicxml"
-    assert GraceNote.from_xml(etree.parse(music_xml_fn).getroot()).glance() == expected_glance
+    grace_note_xml = create_xml_from_file(
+        f"tests/resources/grace_notes/single_notes/slash_{slash}_slur_{slur}.musicxml")
+    assert GraceNote.from_xml(grace_note_xml).glance() == expected_glance
