534b76fcf04ecfb512a40f972ff1d34429ca0e4e
Madeleine Wagner <musomaddy@gmail.com>
2025-09-06 13:19:24 +1000

    Added and tested accidental in the new structure.

---
+++ pymusic/pitch/accidental.py
@@ -0,0 +1,60 @@
+from enum import Enum
+from typing import Optional
+
+from lxml.etree import Element
+
+
+class Accidental(Enum):
+    """ Enum representation of an accidental. """
+
+    def __init__(self, symbol: str, n_semitones: int):
+        self.symbol = symbol
+        self.n_semitones = n_semitones
+
+    SHARP = "♯", 1
+    FLAT = "♭", -1
+    NATURAL = "♮", 0
+    SHARP_2 = "𝄪", 2
+    FLAT_2 = "𝄫", -2
+
+    def glance(self):
+        """ Returns a short easily readable string for this accidental."""
+        if self == Accidental.NATURAL:
+            return ""
+        return self.symbol
+
+    def inversion(self):
+        match self:
+            case Accidental.NATURAL:
+                return Accidental.NATURAL
+            case Accidental.SHARP:
+                return Accidental.FLAT
+            case Accidental.FLAT:
+                return Accidental.SHARP
+            case Accidental.FLAT_2:
+                return Accidental.SHARP_2
+            case Accidental.SHARP_2:
+                return Accidental.FLAT_2
+
+    @staticmethod
+    def from_int(i: int) -> 'Accidental':
+        for accidental in Accidental:
+            if i == accidental.n_semitones:
+                return accidental
+        raise ValueError(f"Unrecognized accidental by {i} semitones")
+
+    @staticmethod
+    def from_str(s: str) -> 'Accidental':
+        # Special case for natural
+        if s == "":
+            return Accidental.NATURAL
+        for accidental in Accidental:
+            if s == accidental.symbol:
+                return accidental
+        raise ValueError(f"Unrecognized accidental with string: {s}")
+
+
+def create_accidental_from_element(element: Optional[Element]) -> Accidental:
+    if element is None:
+        return Accidental.NATURAL
+    return Accidental.from_int(int(element.text))
--- tests/pymusic/pitch/accidentals/test_accidental.py
+++ tests/pymusic/pitch/accidentals/test_accidental.py
@@ -1,5 +1,5 @@
 """ tests for the accidental package. """
-from pymusic.original.pitch.accidentals import Accidental
+from pymusic.pitch.accidental import create_accidental_from_element, Accidental
 from tests import create_xml
 
 
@@ -7,13 +7,20 @@ class TestCorrespondingAccidental:
     """ Tests for corresponding accidental method. """
 
     def test_natural(self):
-        assert Accidental.from_xml(None) == Accidental.NATURAL
+        assert create_accidental_from_element(None) == Accidental.NATURAL
+        assert Accidental.from_int(0) == Accidental.NATURAL
+        assert Accidental.from_str("") == Accidental.NATURAL
+        assert Accidental.from_str("♮") == Accidental.NATURAL
 
     def test_flat(self):
         flat_txt = create_xml(
             "<root-alter>-1</root-alter>")
-        assert Accidental.from_xml(flat_txt) == Accidental.FLAT
+        assert create_accidental_from_element(flat_txt) == Accidental.FLAT
+        assert Accidental.from_int(-1) == Accidental.FLAT
+        assert Accidental.from_str("♭") == Accidental.FLAT
 
     def test_sharp(self):
         sharp_txt = create_xml("<root-alter>1</root-alter>")
-        assert Accidental.from_xml(sharp_txt) == Accidental.SHARP
+        assert create_accidental_from_element(sharp_txt) == Accidental.SHARP
+        assert Accidental.from_int(1) == Accidental.SHARP
+        assert Accidental.from_str("♯") == Accidental.SHARP
